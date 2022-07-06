4ef5db185955c81b9cc129c6495e3ebb58d5e325
KennethJeager25 <95264229+KennethJeager25@users.noreply.github.com>
2022-07-06 14:39:34 -0500

    bot

---
+++ classTempCH2.py
@@ -0,0 +1,46 @@
+from guardarCH2 import jsonFile
+
+class temp_CH2(jsonFile):
+
+    def __init__(self,name="",value="",lista=[]):
+        super().__init__("temp_CH2.json")
+        self.name = name
+        self.value = value
+        self.namefile="temp_CH2.json"
+        self.lista = lista
+
+    def addCh2(self,temp):
+        try:
+            self.lista.append(temp)
+            return "dato guardado"
+        except:
+            return "error al guardar"
+
+    def deleteAllTemp(self):
+        try:
+            for i in self.lista:
+                self.lista.remove(i)
+            return "datos eliminados"
+        except:
+            return "no existen datos"
+
+    def showData(self):
+        try:
+            return self.lista
+        except:
+            return "lista vacia"
+
+    def guardarDatos(self):
+        json_data={}
+        json_data['Temp_CH2']=[]
+        for x in self.lista:
+            arreglo_Json={
+                'name':f'{x.name}',
+                'value':f'{x.value}',
+            }
+            json_data['Temp_CH2'].append(arreglo_Json)
+        self.toJson(json_data)
+
+    def __str__(self):
+        return f"name:{self.name},value:{self.value}"
+    
--- server.py
+++ server.py
@@ -1,8 +1,11 @@
 from selenium import webdriver
 import time
 from classTempCH1 import temp_CH1
+from classTempCH2 import temp_CH2
+from pynput.keyboard import Key, Controller
 
-tank = ''
+keyboard = Controller()
+tank = 'tank:8'
 value = 50
 
 class Bot:
@@ -11,6 +14,7 @@ class Bot:
         self.url = url
         self.navegador = navegador
         self.ch1 = temp_CH1()
+        self.ch2 = temp_CH2()
         
 
     def setTemp(self):
@@ -74,7 +78,7 @@ class Bot:
         if tank == 'tank:8':
             driver.find_element_by_xpath('/html/body/app-root/ion-app/ion-split-pane/ion-router-outlet/app-dashboard/ion-content/ion-grid/ion-row/ion-col[3]/ion-card/div[1]/ion-item-divider/div/div/ion-segment/ion-segment-button[2]').click()
             driver.find_element_by_xpath('/html/body/app-root/ion-app/ion-split-pane/ion-router-outlet/app-dashboard/ion-content/ion-grid/ion-row/ion-col[3]/ion-card/div[1]/ion-card-header/ion-toolbar/ion-buttons[1]/ion-button[1]').click()
-            driver.find_element_by_xpath('/html/body/app-root/ion-app/ion-split-pane/ion-router-outlet/app-dashboard/ion-content/ion-grid/ion-row/ion-col[3]/ion-card/div[1]/ion-card-content/div[2]/div[1]/ion-card[1]/ion-grid/ion-row[2]/ion-col[1]/ion-item/ion-input/input').send_keys(value)
+            #driver.find_element_by_xpath('/html/body/app-root/ion-app/ion-split-pane/ion-router-outlet/app-dashboard/ion-content/ion-grid/ion-row/ion-col[3]/ion-card/div[1]/ion-card-content/div[2]/div[1]/ion-card[1]/ion-grid/ion-row[2]/ion-col[1]/ion-item/ion-input/input').send_keys(value)
             time.sleep(3)
             driver.find_element_by_xpath('/html/body/app-root/ion-app/ion-split-pane/ion-router-outlet/app-dashboard/ion-content/ion-grid/ion-row/ion-col[3]/ion-card/div[1]/ion-card-content/div[2]/div[1]/ion-card[1]/ion-grid/ion-row[2]/ion-col[3]/ion-button/small').click()
             driver.find_element_by_xpath('/html/body/app-root/ion-app/ion-split-pane/ion-router-outlet/app-dashboard/ion-content/ion-grid/ion-row/ion-col[3]/ion-card/div[1]/ion-card-header/ion-toolbar/ion-buttons[1]/ion-button[3]').click()
@@ -94,7 +98,62 @@ class Bot:
 
 
 
-    def showTemp(self):
+    def showTempCH1(self):
+        self.navegador = './chromedriver.exe'
+        self.url='https://mypid.smartpid.com/mypid/'
+
+        driver = webdriver.Chrome(self.navegador)
+        driver.get(self.url)
+
+        driver.find_element_by_xpath('//*[@id="sign-in-container"]/ion-grid/ion-row[1]/ion-col/ion-list/ion-item[1]/ion-input/input').send_keys('marrito@me.com')
+        driver.find_element_by_xpath('//*[@id="sign-in-container"]/ion-grid/ion-row[1]/ion-col/ion-list/ion-item[2]/ion-input/input').send_keys('goodbeer#2022')
+        driver.find_element_by_xpath('//*[@id="sign-in-container"]/ion-grid/ion-row[2]/ion-col/ion-button').click()
+
+        self.url='https://mypid.smartpid.com/mypid/dashboard'
+        driver.refresh()
+
+        driver.execute_script("window.localStorage.setItem('EMAIL', 'marrito@me.com')")
+        driver.execute_script("window.sessionStorage.setItem('MY_PASSWORD', 'goodbeer#2022')")
+        driver.execute_script("window.sessionStorage.setItem('MY_SESSION', 'ALREADY_PRESENT')")
+
+        driver.get(self.url)
+
+        driver.refresh()
+
+        time.sleep(9)
+
+        tank = driver.find_element_by_xpath("/html/body/app-root/ion-app/ion-split-pane/ion-router-outlet/app-dashboard/ion-content/ion-grid/ion-row/ion-col[1]/ion-card/div[1]/ion-card-content/div[1]/div[1]/ion-card/ion-row[2]/ion-col[1]/div[1]/ion-label")
+        tank3 = tank.text.split('°')
+        tempCH1tank3 = temp_CH1()
+        tempCH1tank3.name = 'Tank:3'
+        tempCH1tank3.value = tank3[0]
+        self.ch1.addCh1(tempCH1tank3)
+        tank = driver.find_element_by_xpath("/html/body/app-root/ion-app/ion-split-pane/ion-router-outlet/app-dashboard/ion-content/ion-grid/ion-row/ion-col[2]/ion-card/div[1]/ion-card-content/div[1]/div[1]/ion-card/ion-row[2]/ion-col[1]/div[1]/ion-label")
+        tank5 = tank.text.split('°')
+        tempCH1tank5 = temp_CH1()
+        tempCH1tank5.name = 'Tank:5'
+        tempCH1tank5.value = tank5[0]
+        self.ch1.addCh1(tempCH1tank5)
+        driver.find_element_by_xpath('//*[@id="myFixZone"]/ion-card-header/ion-toolbar/ion-buttons[1]/ion-button[1]').click()
+        driver.find_element_by_xpath('//*[@id="myFixZone"]/ion-card-header/ion-toolbar/ion-buttons[1]/ion-button[3]').click()
+        self.scrolldown()
+        time.sleep(3)
+        tank = driver.find_element_by_xpath("/html/body/app-root/ion-app/ion-split-pane/ion-router-outlet/app-dashboard/ion-content/ion-grid/ion-row/ion-col[3]/ion-card/div[1]/ion-card-content/div[1]/div[1]/ion-card/ion-row[2]/ion-col[1]/div[1]/ion-label")
+        tank7 = tank.text.split('°')
+        tempCH1tank7 = temp_CH1()
+        tempCH1tank7.name = 'Tank:7'
+        tempCH1tank7.value = tank7[0]
+        self.ch1.addCh1(tempCH1tank7)
+        tank = driver.find_element_by_xpath("/html/body/app-root/ion-app/ion-split-pane/ion-router-outlet/app-dashboard/ion-content/ion-grid/ion-row/ion-col[4]/ion-card/div[1]/ion-card-content/div[1]/div[1]/ion-card/ion-row[2]/ion-col[1]/div[1]/ion-label")
+        tank9 = tank.text.split('°')
+        tempCH1tank9 = temp_CH1()
+        tempCH1tank9.name = 'Tank:9'
+        tempCH1tank9.value = tank9[0]
+        self.ch1.addCh1(tempCH1tank9)
+        self.ch1.guardarDatos()
+        time.sleep(3)
+
+    def showTempCH2(self):
         self.navegador = './chromedriver.exe'
         self.url='https://mypid.smartpid.com/mypid/'
 
@@ -116,23 +175,71 @@ class Bot:
 
         time.sleep(9)
 
-        tank = driver.find_element_by_xpath("/html/body/app-root/ion-app/ion-split-pane/ion-router-outlet/app-dashboard/ion-content/ion-grid/ion-row/ion-col[1]/ion-card/div[1]/ion-card-content/div[1]/div[1]/ion-card/ion-row[2]/ion-col[1]/div[1]/ion-label")
-        tank3 = tank.text.split('°')
-        tempCH1tank3 = temp_CH1()
-        tempCH1tank3.name = 'Tank:3'
-        tempCH1tank3.value = tank3[0]
-        self.ch1.addCh1(tempCH1tank3)
-        tank = driver.find_element_by_xpath("/html/body/app-root/ion-app/ion-split-pane/ion-router-outlet/app-dashboard/ion-content/ion-grid/ion-row/ion-col[2]/ion-card/div[1]/ion-card-content/div[1]/div[1]/ion-card/ion-row[2]/ion-col[1]/div[1]/ion-label")
+        driver.find_element_by_xpath('//*[@id="myFixZone"]/ion-item-divider/div/div/ion-segment/ion-segment-button[2]').click()
+        tank = driver.find_element_by_xpath("/html/body/app-root/ion-app/ion-split-pane/ion-router-outlet/app-dashboard/ion-content/ion-grid/ion-row/ion-col[1]/ion-card/div[1]/ion-card-content/div[1]/div[1]/ion-card/ion-row[2]/ion-col[1]/div[2]/ion-label")
         tank4 = tank.text.split('°')
-        tempCH1tank4 = temp_CH1()
+        tempCH1tank4 = temp_CH2()
         tempCH1tank4.name = 'Tank:4'
         tempCH1tank4.value = tank4[0]
-        self.ch1.addCh1(tempCH1tank4)
-        self.ch1.guardarDatos()
-        tank = driver.find_element_by_xpath("/html/body/app-root/ion-app/ion-split-pane/ion-router-outlet/app-dashboard/ion-content/ion-grid/ion-row/ion-col[3]/ion-card/div[1]/ion-card-content/div[1]/div[1]/ion-card/ion-row[2]/ion-col[1]/div[1]/ion-label")
-        print(tank.text)
+        self.ch2.addCh2(tempCH1tank4)
+        driver.find_element_by_xpath('/html/body/app-root/ion-app/ion-split-pane/ion-router-outlet/app-dashboard/ion-content/ion-grid/ion-row/ion-col[2]/ion-card/div[1]/ion-item-divider/div/div/ion-segment/ion-segment-button[2]').click()
+        tank = driver.find_element_by_xpath("/html/body/app-root/ion-app/ion-split-pane/ion-router-outlet/app-dashboard/ion-content/ion-grid/ion-row/ion-col[2]/ion-card/div[1]/ion-card-content/div[1]/div[1]/ion-card/ion-row[2]/ion-col[1]/div[2]/ion-label")
+        tank6 = tank.text.split('°')
+        tempCH1tank6 = temp_CH2()
+        tempCH1tank6.name = 'Tank:6'
+        tempCH1tank6.value = tank6[0]
+        self.ch2.addCh2(tempCH1tank6)
+        self.scrolldown()
+        time.sleep(3)
+        driver.find_element_by_xpath('/html/body/app-root/ion-app/ion-split-pane/ion-router-outlet/app-dashboard/ion-content/ion-grid/ion-row/ion-col[3]/ion-card/div[1]/ion-item-divider/div/div/ion-segment/ion-segment-button[2]').click()
+        tank = driver.find_element_by_xpath("/html/body/app-root/ion-app/ion-split-pane/ion-router-outlet/app-dashboard/ion-content/ion-grid/ion-row/ion-col[3]/ion-card/div[1]/ion-card-content/div[1]/div[1]/ion-card/ion-row[2]/ion-col[1]/div[2]/ion-label")
+        tank8 = tank.text.split('°')
+        tempCH1tank8 = temp_CH2()
+        tempCH1tank8.name = 'Tank:8'
+        tempCH1tank8.value = tank8[0]
+        self.ch2.addCh2(tempCH1tank8)
+        driver.find_element_by_xpath('/html/body/app-root/ion-app/ion-split-pane/ion-router-outlet/app-dashboard/ion-content/ion-grid/ion-row/ion-col[4]/ion-card/div[1]/ion-item-divider/div/div/ion-segment/ion-segment-button[2]').click()
+        tank = driver.find_element_by_xpath("/html/body/app-root/ion-app/ion-split-pane/ion-router-outlet/app-dashboard/ion-content/ion-grid/ion-row/ion-col[4]/ion-card/div[1]/ion-card-content/div[1]/div[1]/ion-card/ion-row[2]/ion-col[1]/div[2]/ion-label")
+        tank10 = tank.text.split('°')
+        tempCH1tank10 = temp_CH2()
+        tempCH1tank10.name = 'Tank:10'
+        tempCH1tank10.value = tank10[0]
+        self.ch2.addCh2(tempCH1tank10)
+        self.ch2.guardarDatos()
+
+    def showSpTempCH1(self):
+        self.navegador = './chromedriver.exe'
+        self.url='https://mypid.smartpid.com/mypid/'
+
+        driver = webdriver.Chrome(self.navegador)
+        driver.get(self.url)
+
+        driver.find_element_by_xpath('//*[@id="sign-in-container"]/ion-grid/ion-row[1]/ion-col/ion-list/ion-item[1]/ion-input/input').send_keys('marrito@me.com')
+        driver.find_element_by_xpath('//*[@id="sign-in-container"]/ion-grid/ion-row[1]/ion-col/ion-list/ion-item[2]/ion-input/input').send_keys('goodbeer#2022')
+        driver.find_element_by_xpath('//*[@id="sign-in-container"]/ion-grid/ion-row[2]/ion-col/ion-button').click()
+
+        self.url='https://mypid.smartpid.com/mypid/dashboard'
+        driver.refresh()
+
+        driver.execute_script("window.localStorage.setItem('EMAIL', 'marrito@me.com')")
+        driver.execute_script("window.sessionStorage.setItem('MY_PASSWORD', 'goodbeer#2022')")
+        driver.execute_script("window.sessionStorage.setItem('MY_SESSION', 'ALREADY_PRESENT')")
+
+        driver.get(self.url)
+
+        driver.refresh()
+
+        time.sleep(9)
 
         
+
+
+
+    def scrolldown(self):
+        for j in range(14):
+            keyboard.press(Key.down)
+            keyboard.release(Key.down)
+
 
 
 if __name__ == '__main__':
@@ -142,6 +249,7 @@ if __name__ == '__main__':
         if validacion == 1:
             botsito.setTemp()
         else:
-            botsito.showTemp()
+            botsito.showTempCH1()
+            botsito.showTempCH2()
     except KeyboardInterrupt:
         print ('Fin del programa')
